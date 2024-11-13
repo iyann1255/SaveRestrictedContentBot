4019d8e48115415e08692801682ec05eef86d10e
iyann1255 <tessbarca1981@gmail.com>
2024-11-14 06:33:47 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("23765163", default=None, cast=int)
+API_HASH = config("008d5313d9006539373c2513373778cd", default=None)
+BOT_TOKEN = config("7249154151:AAG2_V_O3FcVexZUw4qsYWAnY-iKo6PpXnk", default=None)
+SESSION = config("1BVtsOHwBuxB_9O541pVzRwZ-ch1e5xRN5yFdEuxzsDfttYwkAscMdPRurNuHYJU-6kKUXTysKlNV-63llzDKvu6qpP8AuLRXNLFZXRR7R_0Idq3Jw6reWbWzUlygd_SgwmS_fjBgaznXDiB1r3yCcW4hE3OahdsPjzdS3ocZPDCrxyDod9-JVsQuiZ3Otor5h6gj9mFWf8FrGNoLZEvqpBHXqOpoJRyKpO1Zpxsqk5pkLOUxBdPThAN8HMXwAxKlSRx2FD9pY9DeGSQ5fAD6jM24j70SqyEh5g9PTAGaEnUdmOPHY_Hh3JfEv73fbxvBsOWgMQk2nhug-A2fASdKJiOA3XwtNE4=", default=None)
+FORCESUB = config("@", default=None)
+AUTH = config(""6206131497, default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
